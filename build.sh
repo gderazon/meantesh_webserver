26e9860dfdd83f89c37e9cbf7d9a422bd82eb321
Golan Derazon <gderazon@gmail.com>
2014-09-26 17:12:26 +0300

    Fixed build file

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-javac -d bin -sourcepath src -cp ;lib/* src/com/meantesh/*.java src/com/business_logic/*.java tests/com/meantesh/*.java
+javac -d bin -sourcepath src -cp :lib/* src/com/meantesh/*.java src/com/business_logic/*.java tests/com/meantesh/*.java
  
